f1d6c185c3e89ee7b5659c5acbd1235a71899f65
Sahil Singh <sahilsinghmca@gmail.com>
2020-02-29 14:01:34 +0530

    Created Bottom navigation and folder structure changes

---
+++ src/containers/HomeScreen/index.js
@@ -0,0 +1,31 @@
+import 'react-native-gesture-handler';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+
+export default class HomeScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image
+          style={{ width: 250, height: 250 }}
+          source={require("../../assets/images/Ovuli.png")}
+        />
+        <Text style={styles.welcome}>Welcome to Ovuli!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white"
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center"
+  },
+});
---
+++ src/containers/ProfileScreen/index.js
@@ -0,0 +1,27 @@
+import 'react-native-gesture-handler';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+
+export default class ProfileScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Profile</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white"
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center"
+  },
+});
