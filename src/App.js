7bdc38a258f37865ad46251b15416831d77aafaf
Varun Vaibhav <guptavarun619@gmail.com>
2020-03-01 05:11:14 +0530

    Added config for prettier

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import "react-native-gesture-handler";
-import React, { Component } from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { MaterialCommunityIcons } from "react-native-vector-icons";
+import 'react-native-gesture-handler';
+import React, { Component } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons } from 'react-native-vector-icons';
 
-import HistoryScreen from "./screens/HistoryScreen";
-import HomeScreen from "./screens/HomeScreen";
-import ProfileScreen from "./screens/ProfileScreen";
+import HistoryScreen from './screens/HistoryScreen';
+import HomeScreen from './screens/HomeScreen';
+import ProfileScreen from './screens/ProfileScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -17,46 +17,38 @@ export default class App extends Component {
         <Tab.Navigator
           initialRouteName="Home"
           tabBarOptions={{
-            activeTintColor: "#e91e63",
-            style: {}
+            activeTintColor: '#e91e63',
+            style: {},
           }}
         >
           <Tab.Screen
             name="History"
             component={HistoryScreen}
             options={{
-              tabBarLabel: "History",
+              tabBarLabel: 'History',
               tabBarIcon: ({ color, size }) => (
-                <MaterialCommunityIcons
-                  name="history"
-                  color={color}
-                  size={size}
-                />
-              )
+                <MaterialCommunityIcons name="history" color={color} size={size} />
+              ),
             }}
           />
           <Tab.Screen
             name="Home"
             component={HomeScreen}
             options={{
-              tabBarLabel: "Home",
+              tabBarLabel: 'Home',
               tabBarIcon: ({ color, size }) => (
                 <MaterialCommunityIcons name="home" color={color} size={size} />
-              )
+              ),
             }}
           />
           <Tab.Screen
             name="Profile"
             component={ProfileScreen}
             options={{
-              tabBarLabel: "Profile",
+              tabBarLabel: 'Profile',
               tabBarIcon: ({ color, size }) => (
-                <MaterialCommunityIcons
-                  name="account"
-                  color={color}
-                  size={size}
-                />
-              )
+                <MaterialCommunityIcons name="account" color={color} size={size} />
+              ),
             }}
           />
         </Tab.Navigator>
