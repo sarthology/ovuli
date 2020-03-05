ddfcc085df66916d919af1b62478f976a7b31d0b
raven09 <41518238+sushantag9@users.noreply.github.com>
2020-03-05 22:01:37 +0530

    trail

--- src/screens/ProfileScreen/AvgCycleScreen.js
+++ src/screens/ProfileScreen/AvgCycleScreen.js
@@ -1,23 +1,38 @@
 import 'react-native-gesture-handler';
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Image,Picker, TextInput } from 'react-native';
 
 export default class AvgCycleScreen extends Component {
   constructor(props) {
-    super(props);
-    this.state = { text: '' };
-  }
+		super(props);
+		this.state = {
+	  PickerSelection: "NULL"
+		};
+	}
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Enter your Average Cycle Period</Text>
-
         <TextInput
           style={{ height: 40 }}
           placeholder="Type here !"
           onChangeText={text => this.setState({ text })}
-          value={this.state.text}
+          value={this.state.PickerSelection}  
         />
+
+        <Picker
+          selectedValue={this.state.PickerSelection}
+          style={{backgroundColor: "#fafafa" , height: 50, width: 100}}
+          onValueChange={(itemValue, itemIndex) =>this.setState({PickerSelection: itemValue}) }>
+        <Picker.Item label="3 days" value="3" />
+        <Picker.Item label="4 days" value="4" />
+        <Picker.Item label="5 days" value="5" />    
+        <Picker.Item label="6 days" value="6" />
+        <Picker.Item label="7 days" value="7" />
+        <Picker.Item label="8 days" value="8" />
+        <Picker.Item label="9 days" value="9" />
+        <Picker.Item label="10 days" value="10" />
+        </Picker>
       </View>
     );
   }
