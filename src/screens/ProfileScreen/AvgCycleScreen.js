0bf9a2a48bafefa106bbad3eba2cb840cab9d4e9
rits1272 <ritikjain1272@gmail.com>
2020-03-13 16:21:45 +0530

    Update AvgCycleScreen

--- src/screens/ProfileScreen/AvgCycleScreen.js
+++ src/screens/ProfileScreen/AvgCycleScreen.js
@@ -5,7 +5,7 @@ import {
   StyleSheet,
   Image,
   Dimensions,
-  FlatList,
+  Picker,
   TouchableOpacity,
   StatusBar,
 } from 'react-native';
@@ -32,19 +32,45 @@ export default class AvgCycle extends Component {
         <StatusBar hidden />
         <Image source={top} style={styles.top} />
         <Image source={How_long_is_your_cycle} style={styles.cycleText} />
-        <FlatList
-          style={styles.numberList}
-          horizontal
-          data={dates}
-          keyExtractor={(item, index) => index.toString()}
-          renderItem={({ item }) => (
-            <TouchableOpacity onPress={() => this.setState({ cycle: item })}>
-              <Text style={[this.state.cycle === item ? styles.selected : styles.number]}>
-                {item}
-              </Text>
-            </TouchableOpacity>
-          )}
-        />
+        <Picker
+          style={styles.cont}
+          selectedValue={this.state.PickerSelection}
+          itemStyle={{ backgroundColor: '#FADBD8' }}
+          onValueChange={(itemValue, itemIndex) => this.setState({ PickerSelection: itemValue })}
+        >
+          <Picker.Item label="AVERAGE CYCLE DURATION" value="NULL" />
+          <Picker.Item label="10 days" value="10" />
+          <Picker.Item label="11 days" value="11" />
+          <Picker.Item label="12 days" value="12" />
+          <Picker.Item label="13 days" value="13" />
+          <Picker.Item label="14 days" value="14" />
+          <Picker.Item label="15 days" value="15" />
+          <Picker.Item label="16 days" value="16" />
+          <Picker.Item label="17 days" value="17" />
+          <Picker.Item label="18 days" value="18" />
+          <Picker.Item label="19 days" value="19" />
+          <Picker.Item label="20 days" value="20" />
+          <Picker.Item label="21 days" value="21" />
+          <Picker.Item label="22 days" value="22" />
+          <Picker.Item label="23 days" value="23" />
+          <Picker.Item label="24 days" value="24" />
+          <Picker.Item label="25 days" value="25" />
+          <Picker.Item label="26 days" value="26" />
+          <Picker.Item label="27 days" value="27" />
+          <Picker.Item label="28 days" value="28" />
+          <Picker.Item label="29 days" value="29" />
+          <Picker.Item label="30 days" value="30" />
+          <Picker.Item label="31 days" value="31" />
+          <Picker.Item label="32 days" value="32" />
+          <Picker.Item label="33 days" value="33" />
+          <Picker.Item label="34 days" value="34" />
+          <Picker.Item label="35 days" value="35" />
+          <Picker.Item label="36 days" value="36" />
+          <Picker.Item label="37 days" value="37" />
+          <Picker.Item label="38 days" value="38" />
+          <Picker.Item label="39 days" value="39" />
+          <Picker.Item label="40 days" value="40" />
+        </Picker>
         <TouchableOpacity
           style={styles.button}
           onPress={() => this.props.navigation.navigate('LastPeriod')}
@@ -73,31 +99,6 @@ const styles = StyleSheet.create({
     marginBottom: 40,
   },
 
-  numberList: {
-    alignSelf: 'center',
-    width: Dimensions.get('window').width - 50,
-  },
-
-  number: {
-    fontSize: 20,
-    color: '#a3a3a3',
-    margin: 30,
-    textAlign: 'center',
-  },
-
-  selected: {
-    fontSize: 20,
-    color: '#fff',
-    margin: 30,
-    borderWidth: 5,
-    borderColor: '#F77662',
-    backgroundColor: '#F77662',
-    textAlign: 'center',
-    textAlignVertical: 'center',
-    borderRadius: 50,
-    borderRightWidth: 20,
-  },
-
   button: {
     borderWidth: 2,
     width: 120,
@@ -116,5 +117,12 @@ const styles = StyleSheet.create({
     padding: 5,
     paddingLeft: 10,
     color: '#F55963',
+  },
+
+  cont: {
+    backgroundColor: '#FADBD8',
+    height: 50,
+    width: 250,
+    alignSelf: 'center',
   },
 });
