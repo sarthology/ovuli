5b9b8af094d208fb3c82137c3b835e0837f41418
rits1272 <ritikjain1272@gmail.com>
2020-03-17 20:30:23 +0530

    Added Asyncstorage

--- src/screens/ProfileScreen/LastPeriodScreen.js
+++ src/screens/ProfileScreen/LastPeriodScreen.js
@@ -1,15 +1,32 @@
 import 'react-native-gesture-handler';
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, Button, AsyncStorage } from 'react-native';
 import { Calendar } from 'react-native-calendars';
 import moment from 'moment';
 
 export default class LastPeriodScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lastPeriod: moment(new Date()).format('YYYY-MM-DD'),
+    };
+  }
+
+  saveLastPeriod = () => {
+    try {
+      AsyncStorage.setItem('lastPeriod', this.state.lastPeriod);
+    } catch (error) {
+      console.log(error);
+    }
+
+    this.props.navigation.navigate('Dashboard');
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Enter your Last Period</Text>
-
+        {console.log(this.state.lastPeriod)}
         <Calendar
           style={{
             width: 90 + '%',
@@ -18,11 +35,10 @@ export default class LastPeriodScreen extends Component {
             borderColor: 'gray',
           }}
           // Initially visible month. Default = Date()
-          current={Date()}
-          markedDates={moment(new Date()).format('YYYY-MM-DD')}
+          current={this.state.lastPeriod}
           // Handler which gets executed on day press. Default = undefined
           onDayPress={day => {
-            console.log('selected day', day);
+            this.setState({ lastPeriod: day['dateString'] });
           }}
           // Handler which gets executed on day long press. Default = undefined
           onDayLongPress={day => {
@@ -32,7 +48,7 @@ export default class LastPeriodScreen extends Component {
           monthFormat={'dd MM yyyy'}
           // Handler which gets executed when visible month changes in calendar. Default = undefined
           onMonthChange={month => {
-            console.log('month changed', month);
+            this.setState({ lastPeriod: month['dateString'] });
           }}
           // Hide month navigation arrows. Default = false
           // hideArrows={true}
@@ -54,7 +70,7 @@ export default class LastPeriodScreen extends Component {
           // Handler which gets executed when press arrow icon right. It receive a callback can go next month
           onPressArrowRight={addMonth => addMonth()}
         />
-        <Button title="next" onPress={() => this.props.navigation.navigate('Dashboard')} />
+        <Button title="next" onPress={this.saveLastPeriod} />
       </View>
     );
   }
