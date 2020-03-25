c8452ffccfd7a1bf528bff4c3a817633f02cacd0
Sarthak <7lovesharma7@gmail.com>
2020-03-25 18:17:45 +0530

    Changing bad structure

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -11,46 +11,52 @@
 // List of projects/orgs using your project for the users page.
 const users = [
   {
-    caption: 'User1',
+    caption: "User1",
     // You will need to prepend the image path with your baseUrl
     // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: '/img/undraw_open_source.svg',
-    infoLink: 'https://www.facebook.com',
-    pinned: true,
-  },
+    image: "/img/undraw_open_source.svg",
+    infoLink: "https://www.facebook.com",
+    pinned: true
+  }
 ];
 
 const siteConfig = {
-  title: 'Ovuli', // Title for your website.
-  tagline: 'A website for testing',
-  url: 'https://sarthology.github.io', // Your website URL
-  baseUrl: '/Ovuli/', // Base URL for your project */
+  title: "Ovuli", // Title for your website.
+  tagline: "Ovulation Calculator and Calendar 👩🏻‍💻",
+  url: "https://sarthology.github.io/ovuli", // Your website URL
+  baseUrl: "/", // Base URL for your project */
+  // For github.io type URLs, you would set the url and baseUrl like:
+  //   url: 'https://facebook.github.io',
+  //   baseUrl: '/test-site/',
 
-  projectName: 'Ovuli',
-  organizationName: 'sarthology',
+  // Used for publishing and more
+  projectName: "Ovuli",
+  organizationName: "TeamXenox",
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
 
   // For no header links in the top nav bar -> headerLinks: [],
-  headerLinks: [],
+  headerLinks: [
+    { doc: "doc1", label: "Docs" },
+    { doc: "doc4", label: "API" },
+    { page: "help", label: "Help" },
+    { blog: true, label: "Blog" }
+  ],
 
   // If you have users set above, you add it here:
   users,
 
   /* path to images for header/footer */
-  headerIcon: 'img/favicon.ico',
-  footerIcon: 'img/favicon.ico',
-  favicon: 'img/favicon.ico',
+  headerIcon: "img/favicon.ico",
+  footerIcon: "img/favicon.ico",
+  favicon: "img/favicon.ico",
 
   /* Colors for website */
   colors: {
-
-    primaryColor: '#08ada2',
-    secondaryColor: '#057971',
-
+    primaryColor: "#516476",
+    secondaryColor: "#384652"
   },
-
 
   /* Custom fonts for website */
   /*
@@ -71,20 +77,20 @@ const siteConfig = {
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: 'default',
+    theme: "default"
   },
 
   // Add custom scripts here that would be placed in <script> tags.
-  scripts: ['https://buttons.github.io/buttons.js'],
+  scripts: ["https://buttons.github.io/buttons.js"],
 
   // On page navigation for the current documentation page.
-  onPageNav: 'separate',
+  onPageNav: "separate",
   // No .html extensions for paths.
   cleanUrl: true,
 
   // Open Graph and Twitter card images.
-  ogImage: 'img/undraw_online.svg',
-  twitterImage: 'img/undraw_tweetstorm.svg',
+  ogImage: "img/undraw_online.svg",
+  twitterImage: "img/undraw_tweetstorm.svg"
 
   // For sites with a sizable amount of content, set collapsible to true.
   // Expand/collapse the links and subcategories under categories.
