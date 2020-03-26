b41a9151ceea9308db5045c17eea6ef599c60642
iharshit009 <harshitjain1309@gmail.com>
2020-03-26 16:29:57 +0530

    do you know screen

--- src/screens/ProfileScreen/LastPeriodScreen.js
+++ src/screens/ProfileScreen/LastPeriodScreen.js
@@ -1,5 +1,4 @@
 import 'react-native-gesture-handler';
-
 import React, { useEffect } from 'react';
 import {
   StyleSheet,
@@ -13,12 +12,10 @@ import {
 } from 'react-native';
 import CalendarPicker, { CALENDAR_WEEK_DAYS } from 'react-native-calendar-picker';
 import { AntDesign } from '@expo/vector-icons';
-
 import TopImage from '../../assets/images/Last_Period/top.png';
 import { useNavigation } from '@react-navigation/native';
 import * as Font from 'expo-font';
 import AskLastPeriodImage from '@/assets/images/Last_Period/AskLastPeriod.png';
-
 
 const LastPeriodScreen = () => {
   const [selectedDate, setSelectedDate] = React.useState(null);
@@ -122,66 +119,3 @@ const styles = StyleSheet.create({
     backgroundColor: '#F55963',
   },
 });
-
-const LastPeriodScreen = () => {
-  const [selectedDate, setSelectedDate] = React.useState(null);
-  const navigation = useNavigation();
-
-
-  useEffect(() => {
-    Font.loadAsync({
-      'PT-Sans': require('../../../wireframes/assets/fonts/PTC55F.ttf'),
-    });
-  }, []);
-
-  const saveLastPeriod = () => {
-    try {
-      AsyncStorage.setItem('lastPeriod', selectedDate.toISOString());
-    } catch (error) {
-      // console.log(error);
-    }
-
-    navigation.navigate('Dashboard');
-  };
-
-  return (
-    <View style={styles.container}>
-      <StatusBar hidden />
-      <Image style={styles.topImage} source={TopImage} />
-      <View style={{ padding: 20, alignSelf: 'center', flexDirection: 'row' }}>
-        <Text
-          style={[{ fontFamily: 'PT-Sans', fontSize: 30, fontWeight: 'bold', alignSelf: 'center' }]}
-        >
-          When
-        </Text>
-        <Text style={{ fontSize: 25, fontFamily: 'PT-Sans', marginTop: 8 }}>
-          {' '}
-          was your last period?
-        </Text>
-      </View>
-
-      <View style={styles.calenderContainer}>
-        <CalendarPicker
-          onDateChange={date => setSelectedDate(date)}
-          startFromMonday={true}
-          previousTitle=""
-          nextTitle=""
-          headingLevel={0}
-          weekdays={CALENDAR_WEEK_DAYS}
-          dayOfWeekStyles={styles.dayOfWeekStyles}
-          selectedDayStyle={styles.selectedDate}
-          selectedDayTextColor="#fff"
-        />
-      </View>
-      <TouchableOpacity style={styles.button} onPress={saveLastPeriod}>
-        <View style={styles.buttonTextContainer}>
-          <Text style={styles.buttonText}>Finish</Text>
-          <AntDesign style={styles.arrowIcon} name="arrowright" size={18} />
-        </View>
-      </TouchableOpacity>
-    </View>
-  );
-};
-
-
-export default LastPeriodScreen;
