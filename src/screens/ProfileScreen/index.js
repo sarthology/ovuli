ef1e8b03aa36d80ef69994d2e84e1fa184e13962
Samriddhi Agarwal <samriddhiagarwal12@gmail.com>
2020-04-09 19:23:51 +0530

    Changes made to the setting screen
    updated the PR as requested

--- src/screens/ProfileScreen/index.js
+++ src/screens/ProfileScreen/index.js
@@ -1,90 +1,11 @@
 import 'react-native-gesture-handler';
-import React, { Component } from 'react';
-import { 
-  StyleSheet,
-  Text, 
-  View,
-  Dimensions,
-  TouchableHighlight,
-  Image,
- } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, TouchableHighlight, Text, View, Dimensions, Image, Picker } from 'react-native';
 
 import profile from '@wireframes/assets/Setting_screen/profile.png';
 import { Entypo } from 'react-native-vector-icons';
 import { Feather } from 'react-native-vector-icons';
-import { MaterialIcons} from 'react-native-vector-icons';
-
-const COLORS = {
-  grey: 'rgb(179, 182, 183)',
-}
-
-export default class ProfileScreen extends Component {
-  constructor(props) {
-		super(props);
-		this.state = {
-    changeLanguage:"English",
-    changeCycleDays:"20 Days",
-    changeDate: "14 July 2020",
-		};
-  }
-  
-  onPress = () => {
-    this.setState({
-      changeLanguage:"Marathi",
-    })
-  }
-  onPress0 = () => {
-    this.setState({
-      changeCycleDays:"23 Days",
-    })
-  }
-  onPress1 = () => {
-    this.setState({
-      changeDate: "19 June 2020",
-    })
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.head}>Settings{'\n'}</Text>
-
-        <View style={{flexDirection: 'row'}}>
-          <Image source={profile} style={styles.profile} />
-          <Text style={styles.wel}>{'\n\n'}  Samriddhi </Text>
-        </View>
-
-        <View style={styles.design}>
-          <MaterialIcons name="language" style={styles.create} />
-          <Text style={styles.welcome}>  Language                    </Text>
-          <Text >{this.state.language} </Text>
-          <TouchableHighlight onPress={this.onPress}>
-           <Feather name="edit-3" size={20} color= {COLORS.grey}/>
-          </TouchableHighlight>
-        </View>
-
-        <View style={styles.design}>
-          <Entypo name="circular-graph" style={styles.create} />
-          <Text style={styles.welcome}>  Cycle Length              </Text>
-          <Text>{this.state.cycle} </Text>
-          <TouchableHighlight  onPress={this.onPress0}>
-           <Feather name="edit-3" size={20} color= {COLORS.grey}/>
-          </TouchableHighlight>
-        </View>
-
-        <View style={styles.design}>
-          <Feather name="droplet" style={styles.create}/>
-          <Text style={styles.welcome}>  Last Period           </Text>
-          <Text>{this.state.last} </Text>
-          <TouchableHighlight onPress={this.onPress1}>
-           <Feather name="edit-3" size={20} color= {COLORS.grey}/>
-          </TouchableHighlight>
-        </View>
-
-      </View>
-    );
-  }
-}
+import { MaterialIcons } from 'react-native-vector-icons';
 
 const styles = StyleSheet.create({
   container: {
@@ -104,20 +25,21 @@ const styles = StyleSheet.create({
   create: {
     fontSize: 31,
     alignItems: 'stretch',
-    color: '#F08080'
+    color: '#F08080',
   },
   head: {
     fontSize: 35,
-    alignSelf: 'flex-start', 
+    alignSelf: 'flex-start',
   },
-  content: { 
+  content: {
     height: 50,
     width: 150,
   },
   design: {
-    flex: 1, 
+    flex: 1,
     position: 'relative',
     flexDirection: 'row',
+    justifyContent: 'space-evenly',
   },
   profile: {
     alignSelf: 'flex-start',
@@ -128,5 +50,60 @@ const styles = StyleSheet.create({
     bottom: 50,
     left: 0,
   },
-  
+  userInfo: {
+    flexDirection: 'row',
+  },
 });
+
+const COLORS = {
+  grey: 'rgb(179, 182, 183)',
+}
+const ProfileScreen = () => {
+  const [language, setLanguage] = React.useState('English');
+  const [cycle, setCycle] = React.useState('20 Days');
+  const [lastCycle, setLastCycle] = React.useState('14 July 2020');
+
+  console.log(language);
+  console.log(cycle);
+  console.log(lastCycle);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.head}>Settings</Text>
+
+      <View style={styles.userInfo}>
+        <Image source={profile} style={styles.profile} />
+        <Text style={styles.wel}>{'\n\n'}Samriddhi</Text>
+      </View>
+
+      <View style={styles.design}>
+        <MaterialIcons name="language" style={styles.create} />
+        <Text style={styles.welcome}>  Language                    </Text>
+        <Text >{language} </Text>
+          <TouchableHighlight onPress={() => setLanguage('Marathi')}>
+           <Feather name="edit-3" size={20} color= {COLORS.grey}/>
+          </TouchableHighlight>
+      </View>
+
+      <View style={styles.design}>
+        <Entypo name="circular-graph" style={styles.create} />
+        <Text style={styles.welcome}>  Cycle Length             </Text>
+        <Text>{cycle} </Text>
+          <TouchableHighlight  onPress={() => setCycle('23 Days')}>
+           <Feather name="edit-3" size={20} color= {COLORS.grey}/>
+          </TouchableHighlight>
+      </View>
+
+      <View style={styles.design}>
+        <Feather name="droplet" style={styles.create} />
+        <Text style={styles.welcome}> Last Period           </Text>
+        <Text>{lastCycle} </Text>
+          <TouchableHighlight onPress={() => setLastCycle('19 June 2020')}>
+           <Feather name="edit-3" size={20} color= {COLORS.grey}/>
+          </TouchableHighlight>
+      </View>
+    </View>
+  );
+};
+
+export default ProfileScreen;
