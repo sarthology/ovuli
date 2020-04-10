4feeb3215f67c04ff078542070d91084a1c132a5
iharshit009 <harshitjain1309@gmail.com>
2020-04-10 17:44:27 +0530

    added other lang

--- src/i18n.js
+++ src/i18n.js
@@ -3,7 +3,16 @@ import i18n from 'i18n-js';
 // import memoize from ‘lodash.memoize’; // Use for caching/memoize for better performance
 import en from '../languageBase/en.json';
 import Hindi from '../languageBase/hi.json';
-
+import Nepali from '../languageBase/ne.json';
+import Bengali from '../languageBase/be.json';
+import Urdu from '../languageBase/ur.json';
+import Assamese from '../languageBase/as.json';
+import Gujarati from '../languageBase/gu.json';
+import Kannada from '../languageBase/kn.json';
+import Kashmiri from '../languageBase/ks.json';
+import Konkani from '../languageBase/ko.json';
+import Maithili from '../languageBase/ma.json';
+import Malayalam from '../languageBase/ml.json';
 // const locales = RNLocalize.getLocales();
 // if (Array.isArray(locales)) {
 // I18n.locale = locales[0].languageTag;
@@ -12,6 +21,19 @@ import Hindi from '../languageBase/hi.json';
 i18n.defaultLocale = 'en';
 i18n.locale = null;
 i18n.fallbacks = true;
-i18n.translations = { en, Hindi };
+i18n.translations = {
+  en,
+  Hindi,
+  Nepali,
+  Bengali,
+  Urdu,
+  Assamese,
+  Gujarati,
+  Kannada,
+  Kashmiri,
+  Konkani,
+  Maithili,
+  Malayalam,
+};
 
 export default i18n;
