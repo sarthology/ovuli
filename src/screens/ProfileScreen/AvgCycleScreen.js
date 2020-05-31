c0a97717a30b429b8aa2b47a9c8816f80d6cfbc6
PRATEEK PARDESHI <prateekpardeshi6@gmail.com>
2020-05-31 16:25:21 +0530

    made changes in UI

--- src/screens/ProfileScreen/AvgCycleScreen.js
+++ src/screens/ProfileScreen/AvgCycleScreen.js
@@ -155,7 +155,11 @@ const AvgCycle = () => {
       </View>
       <TouchableOpacity style={styles.button} onPress={saveAvgPeriod}>
         <Text style={styles.buttonText}>{i18n.t('continue')}</Text>
-        <AntDesign style={{ alignSelf: 'center', color: '#F55963' }} name="arrowright" size={18} />
+        <AntDesign
+          style={{ alignSelf: 'center', color: '#F55963', top: 2, left: -4 }}
+          name="arrowright"
+          size={18}
+        />
       </TouchableOpacity>
     </View>
   );
